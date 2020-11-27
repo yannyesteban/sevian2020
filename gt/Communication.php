60fa6c427abdf2392b761f32975721d1227f371d
Yanny Núñez <yannyesteban@hotmail.com>
2020-11-27 11:36:05 -0400

    ok

--- gt/Communication.php
+++ gt/Communication.php
@@ -118,7 +118,7 @@ class Communication extends
             'gridId'        => $this->eparams->gridId??'',
             'user'          => $this->_userInfo->user,
             'socketServer'  => [
-                'host'  => 'bests.no-ip.info',
+                'host'  => 'localhost',//'bests.no-ip.info',
                 'port'  => 3321
             ]
         ]);
