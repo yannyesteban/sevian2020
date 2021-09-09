fd5b184def59d29fb470052df86e5c8f186f5d53
Yanny Nunez <yannyesteban@hotmail.com>
2021-09-09 18:15:26 -0400

    rc

--- build/GT/ts/Communication.js
+++ build/GT/ts/Communication.js
@@ -432,6 +432,8 @@ export class Communication {
     reqDataEvent(json) {
         let i = 0;
         const modes = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+        json = json.reverse();
+        console.log(json);
         for (let x in json) {
             i++;
             modes.forEach((m, index) => {
