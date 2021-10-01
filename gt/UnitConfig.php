90cc04cde5ffdfb813f0bb1e442612dc9f12df57
Yanny Esteban <yannyesteban@hotmail.com>
2021-10-01 16:30:35 -0400

    RC8

--- gt/UnitConfig.php
+++ gt/UnitConfig.php
@@ -195,7 +195,7 @@ class UnitConfig
     private function getEvents($unitId){
         $cn = $this->cn;
 
-        $cn->query = "SELECT DISTINCT ue.id,u.id as unit_id, COALESCE(ue.mode, (SELECT e2.mode FROM unit_event as e2 WHERE e2.event_id = ue.event_id limit 1), 0) as `mode`,
+        $cn->query = "SELECT DISTINCT ue.id,u.id as unit_id, COALESCE(ue.mode, (SELECT e2.mode FROM unit_event as e2 WHERE e2.event_id = de.event_id limit 1), 0) as `mode`,
 
         de.event_id, de.name, CASE WHEN ue.unit_id IS NULL THEN 1 ELSE 2 END as __mode_, '' as __record_
         FROM device_event de
